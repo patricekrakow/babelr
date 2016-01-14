6a995f215089f26ea62d47a9fcee2e43f688c89e
Patrice Krakow <patrice.krakow@gmail.com>
2016-01-14 14:09:48 +0100

    Handling connection and deconnection of socket with socket.io!

--- babelr.js
+++ babelr.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/babelr.html');
+});
+
+io.on('connection', function(socket) {
+  var socket_id = socket.id;
+  
+  console.log('The socket ' + socket_id + ' has been established.');
+  
+  socket.on('disconnect', function(){
+    console.log('The socket ' + socket_id + ' has been disconnected.');
+  });
 });
 
 http.listen(80, function() {
